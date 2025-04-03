bc21c60d6edf18fa5cde95032780df4736547d05
Himanshu25102005 <himanshudusane12@gmail.com>
2025-04-04 00:14:44 +0530

    next

--- Script/buy.js
+++ Script/buy.js
@@ -1,1 +1,16 @@
 
+const newsletterForm = document.querySelector('.newsletter-form');
+if (newsletterForm) {
+  newsletterForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const emailInput = this.querySelector('input[type="email"]');
+    const email = emailInput.value.trim();
+    
+    if (email && isValidEmail(email)) {
+      alert('Thank you for subscribing to our newsletter!');
+      emailInput.value = '';
+    } else {
+      alert('Please enter a valid email address.');
+    }
+  });
+}
